4c172d6118a52be9dd54353c91158c48b92a3f48
nishida1125 <larkin.origin@gmail.com>
2019-12-20 11:42:32 +0900

    ImageMagickの実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
+
+    def thumbnail
+        image.variant(resize: '100x100')
+    end
+
     has_one_attached :image
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
     validates :name, presence: true
     validates :phone_number, presence: true
+
+    def thumbnail
+        photo.variant(resize: '100x100')
+    end
+
+
     has_one_attached :photo
 end
